844edd7f5c8609a677ca5d74ca06bec68283b1d3
Endriyas <endriyas@uni.minerva.edu>
2023-11-22 04:29:03 +0900

    complete navbar

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,9 @@ import SearchIcon from "@mui/icons-material/Search";
 import HeaderOption from "./HeaderOption";
 import HomeIcone from "@material-ui/icons/Home";
 import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BussinesCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationIcon from "@material-ui/icons/Notifications";
 // Header component
 function Header() {
   return (
@@ -23,6 +26,13 @@ function Header() {
       <div className="header_right">
         <HeaderOption Icon={HomeIcone} title="Home" />
         <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BussinesCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationIcon} title="Notifications" />
+        <HeaderOption
+          avatar="https://pixabay.com/photos/korea-tree-gyeongbokgung-seoul-7366036/"
+          title="me"
+        />
       </div>
     </div>
   );
--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./HeaderOption.css";
 
-function HeaderOption({ title, Icon }) {
+function HeaderOption({ Avatar, title, Icon }) {
   return (
     <div className="headerOption">
       {Icon && <Icon className="headerOption_icon" />}
+      {Avatar && <Avatar className="headerOption_icon" src={Avatar} />}
       <h3 className="headerOption_title">{title}</h3>
     </div>
   );
