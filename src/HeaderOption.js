4e832abef72053efac284dc7c4b2eb66ff414b74
Endriyas <endriyas@uni.minerva.edu>
2023-11-22 21:50:53 +0900

    side-bar commit

--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./HeaderOption.css";
 
-function HeaderOption({ Avatar, title, Icon }) {
+function HeaderOption({ avatar, title, Icon }) {
   return (
     <div className="headerOption">
       {Icon && <Icon className="headerOption_icon" />}
-      {Avatar && <Avatar className="headerOption_icon" src={Avatar} />}
+      {avatar && <avatar className="headerOption_icon" src={avatar} />}
       <h3 className="headerOption_title">{title}</h3>
     </div>
   );
---
+++ src/Sidebar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Sidebar.css";
+import { Topic } from "@mui/icons-material";
+const Sidebar = () => {
+  const recentItem = (topic) => {
+    return (
+      <div className="sidebar_recentItem">
+        <span className="sidebar_hash">#</span>
+        <p>{topic}</p>
+      </div>
+    );
+  };
+  return (
+    <div className="sidebar">
+      <div className="sidebar_top">
+        <img
+          src="https://images.unsplash.com/photo-1631631480669-535cc43f2327?q=80&w=2187&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        {/* <Avatar className="sidebar_avatar" /> */}
+        <h2>Username</h2>
+        <h4>email</h4>
+      </div>
+      <div className="sidebar_stats">
+        <div className="sidebar_stat">
+          <p>Who viewed you</p>
+          <p className="sidebar_statNumber">2,543</p>
+        </div>
+        <div className="sidebar_stat">
+          <p>Views on post</p>
+          <p className="sidebar_statNumber">2,448</p>
+        </div>
+      </div>
+      <div className="sidebar_bottom">
+        <p>Recent</p>
+        {recentItem("programming")}
+        {recentItem("softwareengineering")}
+        {recentItem("productmangment")}
+        {recentItem("design")}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
